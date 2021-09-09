ca3043e996eba8e549c70e0e61355c28122a8441
StevenGolovkine <steven_golovkine@icloud.com>
2021-09-09 13:03:38 +0200

    Add Gaussian tests

---
+++ scenario_3/022-test_gaussian_assumption.R
@@ -0,0 +1,12 @@
+# Test of the Gaussian assumption
+# based on T. Gorecki, L. Horvath, and P. Kokoszka. Tests of normality of 
+# functional data. International Statistical Review, 88(3):677–697, 2020.
+
+coef <- read.csv('./scenario_3/results/coef.csv', header = FALSE)
+
+test <- MVN::mvn(coef, mvnTest = 'mardia')
+test <- MVN::mvn(coef, mvnTest = 'royston')
+test <- MVN::mvn(coef, mvnTest = 'hz')
+test <- MVN::mvn(coef, mvnTest = 'energy')
+test <- normwhn.test::normality.test1(coef)
+test <- normwhn.test::normality.test2(coef)
